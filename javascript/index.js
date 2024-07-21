7dd853721d52030391f52b82cedb6e533a5c7de8
JodiS34 <147281995+JodiS34@users.noreply.github.com>
2024-07-21 00:58:16 -0400

    Updated index.js
    I changed the code for index.js to see if that would fix its breaks.

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,4 @@
-setInterval(function () {
+function updateLosAngelesTime () {
   let losAngelesElement = document.querySelector("#los-Angles");
   let losAngelesDateElement = losAngelesElement.querySelector(".date");
   let losAngelesTimeElement = losAngelesElement.querySelector(".time");
@@ -9,7 +9,8 @@ setInterval(function () {
   losAngelesTimeElement.innerHTML = losAnglesTime.format(
     "h:mm:ss[<small>]A[</small>]"
   );
-}, 1000);
+
+    setInterval(updateLosAngelesTime, 1000);
 let SydneyElement = document.querySelector("#Sydney");
 let SydneyDateElement = SydneyElement.querySelector(".date");
 let SydneyTimeElement = SydneyElement.querySelector(".time");
