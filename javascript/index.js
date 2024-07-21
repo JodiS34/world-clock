b1f84869fc9a06897bc10e1062f8dd95be75b3bc
JodiS34 <147281995+JodiS34@users.noreply.github.com>
2024-07-21 00:48:26 -0400

    Updated index.js
    I wrote the index.js code for Los Angeles and Sydney.

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,20 @@
-alert("hello");
+setInterval(function () {
+  let losAngelesElement = document.querySelector("#los-Angles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAnglesTime = moment().tz("America/Los_Angles");
+
+  losAngelesDateElement.innerHTML = losAnglesTime.format("MMMM Do YYYY");
+
+  losAngelesTimeElement.innerHTML = losAnglesTime.format(
+    "h:mm:ss[<small>]A[</small>]"
+  );
+}, 1000);
+let SydneyElement = document.querySelector("#Sydney");
+let SydneyDateElement = SydneyElement.querySelector(".date");
+let SydneyTimeElement = SydneyElement.querySelector(".time");
+let SydneyTime = moment().tz("Australia/Sydney");
+
+SydneyDateElement.innerHTML = SydneyTime.format("MMMM Do YYYY");
+
+SydneyTimeElement.innerHTML = SydneyTime.format("h:mm:ss[<small>]A[</small>]");
